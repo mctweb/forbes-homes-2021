d7959318bc2848c03bc98c83c85b998d76f40df1
fmctaggart <fraser@mctweb.co.uk>
2021-08-28 12:57:11 +0100

    Changing image and cleanup

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <div>
     <SectionTopBlue>
       <PageHeader> About Us</PageHeader>
-      <HeaderImageHero title="About Forbes Homes" :image="mainImages[0].src">
+      <HeaderImageHero title="About Forbes Homes" image="/deeview/deeview.png">
         We are a family managed business with over 30 years' experience in the construction industry.
       </HeaderImageHero>
 
@@ -37,12 +37,10 @@ import { getSiteMeta } from '~/utils/getSiteMeta'
 export default {
   data () {
     return {
-      mainImages: [{ src: '/stripeside/Stripeside-27.png', room: 'exterior', featured: true }, { src: '/stripeside/Stripeside-25.png', room: 'exterior', featured: true }, { src: '/stripeside/Stripeside-20.png', room: 'exterior', featured: true }],
       quote: {
         message: 'Forbes Homes are not your average building company - they are a long standing family run company with their roots firmly in the North East of Scotland. They take a real pride in their work whether it be new build ( commercial and residential ) or renovations / extensions . Their experienced team of specialists covers all trades and the quality of workmanship is second to none . I have no hesitation in recommending their services .',
         author: 'Dave',
         authorTitle: 'Homeowner'
-
       }
     }
   },
