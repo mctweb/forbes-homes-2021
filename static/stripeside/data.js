704177acf9c54c7b54220cb13ca589fdf7cc31b0
fmctaggart <fraser@mctweb.co.uk>
2022-01-10 10:39:08 +0000

    Change spec for remaining properties

--- static/stripeside/data.js
+++ static/stripeside/data.js
@@ -7,28 +7,6 @@ const allPlots = {
   image: '/stripeside/basic.png',
   siteLayout: 'StripesideSiteLayout',
   price: '',
-  features: [
-    {
-      title: 'Modern & Spacious Kitchen',
-      description: 'Clean lines, exquisite materials and clearly structured architecture – these are the distinguishing marks of this designer Nobilia kitchen. Integrated appliances, with solid quartz worktops make for a modern and polished finish.',
-      image: '/stripeside/Stripeside-47.png'
-    },
-    {
-      title: 'Enviable Lounge',
-      description: 'No matter what size of home, the lounge is the place where people can relax, chat, and enjoy each other\'s company. This spacious living room ensures that the family can enjoy time together in comfort with plenty of natural light.',
-      image: '/stripeside/Stripeside-48.png'
-    },
-    {
-      title: 'Five Generous Bedrooms',
-      description: 'A master bedroom with en suite and walk in wardrobe. Two further bedrooms with en suites and walk in wardrobes. In addition to two standard bedrooms.',
-      image: '/stripeside/Stripeside-46.png'
-    },
-    {
-      title: 'Five Luxurious Bathrooms',
-      description: 'Our Stripeside homes come with five fully fitted bathrooms.',
-      image: '/stripeside/Stripeside-04.png'
-    }
-  ],
   mapLocation: {
     lat: 57.05577368908729,
     lng: -2.2209145018531053
@@ -52,44 +30,68 @@ const allPlots = {
   specification: [
     {
       title: 'Kitchen',
-      description: 'Nobila furniture, Neff appliances with quartz worktop as per viewing home – supplied and installed by Laings Inverurie.',
+      description: 'Choice of high specification furniture, Neff appliances with quartz worktop as per viewing home.',
       list: [
         'Double oven', 'Induction hob', 'Extractor canopy', 'Integral fridge freezer', 'Dishwasher'
       ]
     },
-    { title: 'Utility', description: 'Nobila furniture with 40mm laminate worktop as per viewing home – supplied and installed by Laings Inverurie.' },
+    { title: 'Dining Room', description: 'Open plan dining room with log burning stove.' },
+    { title: 'Utility', description: 'Choice of high specification furniture with 40mm laminate worktop as per viewing home.' },
     { title: 'Carpets', description: 'Fitted in living room, bedrooms and dressing rooms. Colour samples are available to select from.' },
-    { title: 'Laminate Flooring', description: 'Plank laminate flooring in hall, kitchen and utility areas and also available in all shower room, en suite and bathroom areas. Samples available to select from.' },
-    { title: 'Underfloor Heating', description: 'The ground floor has underfloor heating throughout. On the upper floors, electric under floor heating is fitted in the bathroom and en suite shower rooms.' },
+    { title: 'Luxury Vinyl Flooring', description: 'Kardean vinyl flooring in hall, kitchen and utility areas and also available in all shower room, en suite and bathroom areas. Samples available to select from.' },
+    { title: 'Underfloor Heating', description: 'The ground floor has underfloor heating throughout. On the 1st floor, electric under floor heating is fitted in the bathroom and en suite shower rooms.' },
     { title: 'Tiles', description: 'Wall tiling as per viewing home – tiles supplied from Aberdeen Tile Distributors. Also available for all shower rooms, en suite and bathroom floors.' },
     {
-      title: 'Bathroom & En Suite sanitaryware',
+      title: 'Bathroom & En Suite Sanitaryware',
       description: '(as per viewing home)',
       list: [
-        'Wall hung Kohler or Roca toilet pans (concealed cistern already installed)',
-        'The ground floor shower and main bathroom have single wash hand basins including vanity unit',
+        'Wall hung Roca toilet pans (concealed cistern already installed)',
+        'The ground floor shower, main bathroom and bedroom 2 ensuite have single wash hand basins including vanity unit',
         'The master en suite is piped for a double wash hand basin with option of vanity unit below',
-        'The second floor en suites have semi pedestal wash hand basins',
         'LED mirrors above all wash hand basins',
         'Fixed head and slide rails fitted in each shower with Zen wet room fixed glass panels',
         'Free standing bath in bathroom as per viewing home',
         'Towel rails in each en suite, bathroom and shower room',
-        'Electric underfloor heating mats in first and second floor en suites and bathroom.'
+        'Electric underfloor heating mats in first floor en suites and bathroom.'
       ]
     },
     { title: 'Window Blinds', description: 'Roller blinds installed as per viewing home.' },
     {
       title: 'Sound System',
-      description: 'The following rooms have been hard wired for ceiling speakers which will be installed.   All speakers are wired back to the cupboard in the kitchen where the Sonos amps/controllers are located',
+      description: 'The following rooms have been hard wired for ceiling speakers which will be installed.  All speakers are wired back to the cupboard in the 1st floor landing where the Sonos amps/controllers are located',
       list: [
         'Living room',
         'Kitchen/Dining',
         'Master bedroom and master en suite',
         'Bathroom',
+        'First floor hall',
         'All remaining bedrooms'
       ]
     }
+  ],
+  features: [
+    {
+      title: 'Modern & Spacious Kitchen',
+      description: 'Clean lines, exquisite materials and clearly structured architecture – these are the distinguishing marks of this designer kitchen. Integrated appliances, with solid quartz worktops make for a modern and polished finish.',
+      image: '/stripeside/Stripeside-47.png'
+    },
+    {
+      title: 'Enviable Lounge',
+      description: 'No matter what size of home, the lounge is the place where people can relax, chat, and enjoy each other\'s company. This spacious living room ensures that the family can enjoy time together in comfort with plenty of natural light.',
+      image: '/stripeside/crossley/lounge.png'
+    },
+    {
+      title: 'Four Generous Bedrooms',
+      description: 'A master bedroom with spacious dressing room and ensuite, bedroom and ensuite, plus 2 further bedrooms.',
+      image: '/stripeside/Stripeside-46.png'
+    },
+    {
+      title: 'Four Luxurious Bathrooms',
+      description: 'The Crossley style of home come with 4 fully fitted bathrooms.',
+      image: '/stripeside/crossley/bathroom.png'
+    }
   ]
+
 }
 
 const crossley = {
